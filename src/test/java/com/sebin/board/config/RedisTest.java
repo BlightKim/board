6515b34dd832d30f4fdb7f9b111bf0949122e81f
ksb96 <ksebin96@gmail.com>
2023-07-30 20:12:37 +0900

    commit

--- src/test/java/com/sebin/board/config/RedisTest.java
+++ src/test/java/com/sebin/board/config/RedisTest.java
@@ -1,4 +1,19 @@
+package com.sebin.board.config;
+
 import static org.junit.jupiter.api.Assertions.*;
-class RedisTest {
-  
+
+import com.sebin.board.config.code.AbstractContainerBaseTest;
+import com.sebin.board.service.RedisService;
+import com.sebin.board.service.RedisServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedisTest extends AbstractContainerBaseTest {
+  @Autowired
+  private RedisTemplate<String, Object> redisTemplate;
+  @Autowired
+  private RedisService redisService = new RedisServiceImpl(redisTemplate)
+
 }
